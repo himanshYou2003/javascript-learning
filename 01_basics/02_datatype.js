5f2f56f558e203c9f743713c8dc247adcaaef824
himanshYou2003 <117570353+himanshYou2003@users.noreply.github.com>
2024-02-03 05:52:01 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) //we are using nodejs not browser
+
+//code redeabitily should be high
+
+//number : 2 to power 53
+//bigint :
+//string : ""
+//boolean : ture / false
+//null :standalone value //represent of empty value
+//undefine : 
+//symbol  : unique
+
+//object
+
+console.log(typeof "Zany");
+console.log(typeof null);   //object
+console.log(typeof undefined); //undefined
+
