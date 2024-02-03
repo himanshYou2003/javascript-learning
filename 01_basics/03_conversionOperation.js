c501f0fbc542bb6326810d808a6e9db02bbe0a0a
himanshYou2003 <117570353+himanshYou2003@users.noreply.github.com>
2024-02-03 06:29:04 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,32 @@
 let score ="33"
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
 let changeIntoNumber = Number(score)
 
-console.log(typeof(changeIntoNumber));
+// console.log(typeof(changeIntoNumber));
 
 //  "33" : 33
 //  "33abc" : NaN  (not  a number)  but still it can changes by Number into number
 //  true    : 1;  false : 0;
+
+
+//-------------------O-P-E-R-A-T-I-O-N---------------------------------------------------------
+
+let num = 3
+let negative = -num
+// console.log(negative);
+
+
+
+let str1 = "Hello"
+let str2 = " Himanshu"
+let str3 = str1+str2;
+
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
