095cf370443daba5b5e55501949ba5000a9e6da3
himanshYou2003 <117570353+himanshYou2003@users.noreply.github.com>
2024-02-03 09:14:57 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,24 @@
+const name = "himanshu-kumar"
+const repoCount = 61
+
+// console.log(`hi my name is ${name} and my roll no is ${repoCount}`);
+
+const myName = new String("Himanshu")
+
+// console.log(myName[0]);
+// console.log(myName.toUpperCase());
+// console.log(myName.charAt(5));
+// console.log(myName.indexOf(5));
+
+const newString = myName.substring(0,5) //we cant give negative 
+// console.log(newString);
+
+const anotherString = myName.slice(-7,4)
+// console.log(anotherString);
+dynamically
+const trimString ="     Hima        "
+console.log(trimString);
+console.log(trimString.trim());
+
+const url = "https://himanshukumar.com/frontend/javascript%61HTML"
+// console.log(url.replace('%61','-'))
