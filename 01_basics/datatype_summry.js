58e0de9410e33930fada0fe5b32ec56086566f04
himanshYou2003 <117570353+himanshYou2003@users.noreply.github.com>
2024-02-03 07:16:02 +0000

    memory

--- 01_basics/datatype_summry.js
+++ 01_basics/datatype_summry.js
@@ -19,9 +19,47 @@ const myFunction = function(){
 
 
 
-
 //javascript dynamically type launguage coz you cant give the int,boolean like this
 
-let first = symbol("123")
-let second = symbol("123")
-console.log(first===second); //answer will error coz symbol are unique so there dtatatype not same
+// let first = symbol("123")
+// let second = symbol("123")
+// console.log(first===second); //answer will error coz symbol are unique so there dtatatype not same
+
+
+
+
+//-----------------------------------M-E-M-O-R-Y------------------------------------------------
+
+//      Stack(primative)                                        Heap(non-primative)
+
+//      value(change in as copy )                          refrence[change in original value]
+
+
+
+//----------------------------------------------S-T-A-C-K-------------------------------------
+let myName = "himanshu"
+
+let nickName = myName
+
+//---change 
+
+nickName = "zany"
+
+// console.log(nickName);
+// console.log(myName);
+
+
+
+//-----------------------------------------------H-E-A-P---------------------------------------
+
+let user1 = {
+    email : "himanshu@gmail.com",
+    upi : "himanshu@ylb"
+}
+let user2 = user1
+
+
+user2.email="zany@gmail.com"
+
+console.log(user2.email);
+console.log(user1.email);
