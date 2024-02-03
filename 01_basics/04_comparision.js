d8f9f6325692ac1f3565f672dcab377ecbafbdd3
himanshYou2003 <117570353+himanshYou2003@users.noreply.github.com>
2024-02-03 06:30:48 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2!=1);
+
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null==0); //the reason is that in eqality check== and comparision<>= work differently
+// console.log(null>=0);   //comparision convert to a number and act as 0
+
+// console.log(undefined<0);
+// console.log(undefined==0);
+// console.log(undefined>0);
+
+//  ===  strict check with detatype
+console.log("2"==2);
+console.log("2"===2);
