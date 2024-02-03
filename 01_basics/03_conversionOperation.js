908ee9b18d325724ad431b73123c21deb1f9648b
himanshYou2003 <117570353+himanshYou2003@users.noreply.github.com>
2024-02-03 05:52:31 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let score ="33"
+
+console.log(typeof(score));
+
+let changeIntoNumber = Number(score)
+
+console.log(typeof(changeIntoNumber));
+
+//  "33" : 33
+//  "33abc" : NaN  (not  a number)  but still it can changes by Number into number
+//  true    : 1;  false : 0;
