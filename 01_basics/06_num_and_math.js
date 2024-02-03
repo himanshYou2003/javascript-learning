3e3ee60e0e62d0eec8bba3dc229e48405ec8bbbe
himanshYou2003 <117570353+himanshYou2003@users.noreply.github.com>
2024-02-03 10:27:03 +0000

    number_and_math

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,36 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// const changeString = balance.toString()
+// console.log(changeString);
+// console.log(typeof changeString);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds  = 100000
+// console.log(hundreds.toLocaleString('en-In'));
+
+
+
+
+//--------------------------------M-A-T-H--------------------------------------------------------
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(45.6));
+// console.log(Math.ceil(45.1));
+// console.log(Math.floor(45.9));
+// console.log(Math.min('7','3','1','9'));
+// console.log(Math.max('7','3','1','9'));
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max -min +1))+min );
